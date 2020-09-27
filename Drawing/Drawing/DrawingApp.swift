b72d517f23158c4ad712c19e85750b71388f6539
Ania <zamora1607@users.noreply.github.com>
2020-09-27 15:06:31 +0200

    Add Drawings project - finish day 43

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Ania on 26/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
