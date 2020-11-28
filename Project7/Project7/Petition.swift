8446852c858433b727f30c2c8dc648afeccc9313
Ania <zamora1607@users.noreply.github.com>
2020-11-28 10:07:34 +0100

    Project7 finished day 33 from 100DaysOfSwift

---
+++ Project7/Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Ania on 28/11/2020.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project7/Project7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Ania on 28/11/2020.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
