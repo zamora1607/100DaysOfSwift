5723b0b4f5038ab18f57074317be414d65cad572
Ania <zamora1607@users.noreply.github.com>
2020-12-16 07:31:31 +0100

    Finish day 69 - MapKit basic usage and FaceID/TouchId integration

---
+++ BucketList/BucketList/MapView.swift
@@ -0,0 +1,58 @@
+//
+//  MapView.swift
+//  BucketList
+//
+//  Created by Ania on 16/12/2020.
+//
+
+import MapKit
+import SwiftUI
+
+struct MapViewWrapper: View {
+   
+    var body: some View {
+        MapView()
+        .ignoresSafeArea(.all)
+    }
+}
+
+struct MapView: UIViewRepresentable {
+    func makeUIView(context: UIViewRepresentableContext<MapView>) -> some MKMapView {
+        let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        
+        let annotation = MKPointAnnotation()
+        annotation.title = "London"
+        annotation.subtitle = "Capital of England"
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 51.5, longitude: 0.13)
+        mapView.addAnnotation(annotation)
+        
+        return mapView
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        //
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: MapView
+        
+        init(_ parent: MapView) {
+            self.parent = parent
+        }
+        
+        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+            print(mapView.centerCoordinate)
+        }
+        
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            let view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: nil)
+            view.canShowCallout = true
+            return view
+        }
+    }
+}
