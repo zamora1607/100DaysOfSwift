11e6e5942a72cba74d13e2a4a0541c433883add0
Ania <zamora1607@users.noreply.github.com>
2020-12-20 14:50:02 +0100

    Finish ay 73 - Bucket List Wrap up

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -9,57 +9,44 @@ import SwiftUI
 import MapKit
 import LocalAuthentication
 
-struct ContentView: View {
-    
+struct MapViewScreen: View {
     @State private var centerCoordinate = CLLocationCoordinate2D()
-    @State private var locations = [CodableMKPointAnnotation]()
     @State private var selectedPlace: MKPointAnnotation?
+    @State private var locations = [CodableMKPointAnnotation]()
     @State private var showingPlaceDetails = false
     @State private var showingEditScreen = false
-    @State private var isUnlocked = false
     
     var body: some View {
         ZStack {
-            if isUnlocked {
-                MapView(centerCoordinate: $centerCoordinate, selectedPlace: $selectedPlace, showingPlacesDetails: $showingPlaceDetails, annotations: locations)
-                    .edgesIgnoringSafeArea(.all)
-                Circle()
-                    .fill(Color.blue)
-                    .opacity(0.3)
-                    .frame(width: 32, height: 32)
-                VStack {
+            MapView(centerCoordinate: $centerCoordinate, selectedPlace: $selectedPlace, showingPlacesDetails: $showingPlaceDetails, annotations: locations)
+                .edgesIgnoringSafeArea(.all)
+            Circle()
+                .fill(Color.blue)
+                .opacity(0.3)
+                .frame(width: 32, height: 32)
+            VStack {
+                Spacer()
+                HStack {
                     Spacer()
-                    HStack {
-                        Spacer()
-                        Button(action: {
-                            let newLocation = CodableMKPointAnnotation()
-                            newLocation.coordinate = self.centerCoordinate
-                            newLocation.title = "Example location"
-                            newLocation.subtitle = "Example text"
-                            self.locations.append(newLocation)
-                            self.selectedPlace = newLocation
-                            self.showingEditScreen = true
-                        })
-                        {
-                            Image(systemName: "plus")
-                        }
-                        .padding()
-                        .background(Color.black.opacity(0.75))
-                        .foregroundColor(.white)
-                        .font(.title)
-                        .clipShape(Circle())
-                        .padding(.trailing)
+                    Button(action: {
+                        let newLocation = CodableMKPointAnnotation()
+                        newLocation.coordinate = self.centerCoordinate
+                        newLocation.title = "Example location"
+                        newLocation.subtitle = "Example text"
+                        self.locations.append(newLocation)
+                        self.selectedPlace = newLocation
+                        self.showingEditScreen = true
+                    })
+                    {
+                        Image(systemName: "plus")
+                            .padding()
+                            .background(Color.black.opacity(0.75))
+                            .foregroundColor(.white)
+                            .font(.title)
+                            .clipShape(Circle())
+                            .padding(.trailing)
                     }
                 }
-            } else {
-                Button("Unlock Places") {
-                    self.authenticate()
-                }
-                .padding()
-                .background(Color.blue)
-                .foregroundColor(.white)
-                .clipShape(Capsule())
-                
             }
         }
         .alert(isPresented: $showingPlaceDetails) {
@@ -101,6 +88,32 @@ struct ContentView: View {
             print("Unable to save data")
         }
     }
+}
+
+struct ContentView: View {
+    
+    @State private var isUnlocked = false
+    @State private var showingAuthenticationError = false
+    @State private var errorMsg = ""
+    
+    var body: some View {
+        ZStack {
+            if isUnlocked {
+                MapViewScreen()
+            } else {
+                Button("Unlock Places") {
+                    self.authenticate()
+                }
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .clipShape(Capsule())
+            }
+        }
+        .alert(isPresented: $showingAuthenticationError) {
+            Alert(title: Text("Error"), message: Text(errorMsg), dismissButton: .default(Text("OK")))
+        }
+    }
     
     func authenticate() {
         let context = LAContext()
@@ -114,10 +127,15 @@ struct ContentView: View {
                     if success {
                         self.isUnlocked = true
                     } else {
+                        self.errorMsg = "Authentication failed"
+                        self.showingAuthenticationError = true
+                        
                     }
                 }
             }
         } else {
+            self.errorMsg = "Cannot evaluate policy with biometrics"
+            self.showingAuthenticationError = true
         }
     }
 }
