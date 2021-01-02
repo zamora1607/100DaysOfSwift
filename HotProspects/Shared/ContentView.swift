96522dc40a9e16c1d2f62af1bf9d9a8efe29247d
Ania <zamora1607@users.noreply.github.com>
2021-01-02 16:30:02 +0100

    TabBar, @EnvirinmentObject, dynamically filtering a SwiftUI list - finish day 82

--- HotProspects/Shared/ContentView.swift
+++ HotProspects/Shared/ContentView.swift
@@ -6,81 +6,37 @@
 //
 
 import SwiftUI
-import UserNotifications
-import SamplePackage
 
 
 struct ContentView: View {
     
-    let possibleNumbers = Array(1...60)
-    
-    var results: String {
-        let selected = possibleNumbers.random(7).sorted()
-        let strings = selected.map(String.init)
-        return strings.joined(separator: ", ")
-    }
+    var prospects = Prospects()
     
     var body: some View {
-        Text(results)
+        TabView {
+            ProspectView(filter: .none)
+                .tabItem {
+                    Image(systemName: "person.3")
+                    Text("Everyone")
+                }
+            ProspectView(filter: .contacted)
+                .tabItem {
+                    Image(systemName: "checkmark.circle")
+                    Text("Contacted")
+                }
+            ProspectView(filter: .uncontacted)
+                .tabItem {
+                    Image(systemName: "questionmark.diamond")
+                    Text("Uncontacted")
+                }
+            MeView()
+                .tabItem {
+                    Image(systemName: "person.crop.square")
+                    Text("Me")
+                }
+        }.environmentObject(prospects)
     }
 }
-
-//struct ContentView: View {
-//
-//    @State private var backgroundColor = Color.red
-//
-//    var body: some View {
-//        VStack {
-//            Button("Request Permission") {
-//                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (success, error) in
-//                    if success {
-//                        print("All set")
-//                    } else if let error = error {
-//                        print(error.localizedDescription)
-//                    }
-//                }
-//            }
-//            .padding()
-//            Button("Schedule notification") {
-//                let content = UNMutableNotificationContent()
-//                content.title = "Feed the cat"
-//                content.subtitle = "It looks hungry"
-//                content.sound = UNNotificationSound.default
-//
-//                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-//
-//                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-//
-//                UNUserNotificationCenter.current().add(request)
-//            }
-//            .padding()
-//            Spacer()
-//            Text("Henlo world!")
-//                .padding()
-//                .background(backgroundColor)
-//            Text("Change color")
-//                .padding()
-//                .contextMenu{
-//                    Button(action: {
-//                        self.backgroundColor = .red
-//                    }) {
-//                        Text("Red")
-//                        Image(systemName: "checkmark.circle.fill")
-//                    }
-//                    Button(action: {
-//                        self.backgroundColor = .green
-//                    }) {
-//                        Text("Green")
-//                    }
-//                    Button(action: {
-//                        self.backgroundColor = .blue
-//                    }) {
-//                        Text("Blue")
-//                    }
-//                }
-//        }
-//    }
-//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ HotProspects/Shared/ProspectView.swift
@@ -0,0 +1,72 @@
+//
+//  ProspectView.swift
+//  HotProspects
+//
+//  Created by Ania on 02/01/2021.
+//
+
+import SwiftUI
+
+enum FilterType {
+    case none, contacted, uncontacted
+}
+
+struct ProspectView: View {
+    
+    @EnvironmentObject var prospects: Prospects
+    
+    let filter: FilterType
+    
+    var title: String {
+        switch filter {
+        case .none:
+            return "Everyone"
+        case .contacted:
+            return "Contacted people"
+        case .uncontacted:
+            return "Uncontacted people"
+        }
+    }
+    
+    var filteredProspects: [Prospect] {
+        switch filter {
+        case .none:
+            return prospects.people
+        case .contacted:
+            return prospects.people.filter { $0.isContacted }
+        case .uncontacted:
+            return prospects.people.filter { !$0.isContacted }
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(filteredProspects) { prospect in
+                    VStack(alignment: .leading) {
+                        Text(prospect.name)
+                            .font(.headline)
+                        Text(prospect.emailAddress)
+                            .foregroundColor(.secondary)
+                    }
+                }
+            }
+                .navigationBarTitle(title)
+                .navigationBarItems(trailing: Button(action: {
+                    let prospect = Prospect()
+                    prospect.name = "Paul Hudson"
+                    prospect.emailAddress = "paul@hackingwithswift.com"
+                    self.prospects.people.append(prospect)
+                }){
+                    Image(systemName: "qrcode.viewfinder")
+                    Text("Scan")
+                })
+        }
+    }
+}
+
+struct ProspectView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProspectView(filter: .none)
+    }
+}
